c39f9ce033cbd36331efaf7e39f157a2e26f047d
AndreasMuncier <olly_s@hotmail.co.uk>
2017-11-29 19:05:53 +0000

    Added memory tracking inits to test

--- coursework2/UnitTests/main.c
+++ coursework2/UnitTests/main.c
@@ -8,6 +8,14 @@
 #include "test_header.h"
 
 int main() {
+    
+    initMemTracking();
+    if ( initFiltErrHandling() != FILT_NO_ERR ) {
+        programExit( BAD_FILE_OPEN, "Error initialising filter memory temporary file." );
+    }
+//    initFileTracking();
+    
+    
     qtestsuite_t * testsuite = create_qtestsuite( "Test Suite" );
     
     addFilterTests( testsuite );
